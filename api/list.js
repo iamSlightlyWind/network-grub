afe196ffc66bbf9d0b2fe133057ba56dc15f9ba2
iamSlightlyWind <70873548+iamSlightlyWind@users.noreply.github.com>
2025-04-24 13:43:01 +0700

    load repo dynamically

--- api/list.js
+++ api/list.js
@@ -1,6 +1,14 @@
 export default async function handler(req, res) {
     try {
-        const repoUrl = 'https://api.github.com/repos/iamSlightlyWind/network-grub/contents';
+        const repoOwner = process.env.VERCEL_GIT_REPO_OWNER;
+        const repoSlug = process.env.VERCEL_GIT_REPO_SLUG;
+
+        if (!repoOwner || !repoSlug) {
+            throw new Error('Vercel environment variables for the repository are not set.');
+        }
+
+        const repoUrl = `https://api.github.com/repos/${repoOwner}/${repoSlug}/contents`;
+        console.log(`Fetching repository contents from: ${repoUrl}`);
 
         const response = await fetch(repoUrl, {
             headers: {
@@ -16,7 +24,7 @@ export default async function handler(req, res) {
 
         const cfgFiles = files
             .filter(file => file.name.endsWith('.cfg'))
-            .map(file => file.name.replace('.cfg', ''))
+            .map(file => file.name.replace('.cfg', ''));
 
         res.status(200).json(cfgFiles);
     } catch (error) {
