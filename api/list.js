7fbb67f5e31bbba90e9cd96fc10cb260c3304d44
iamSlightlyWind <70873548+iamSlightlyWind@users.noreply.github.com>
2025-04-24 15:18:04 +0700

    Return error instead of throwing

--- api/list.js
+++ api/list.js
@@ -1,18 +1,17 @@
+import { checkValidRepo, parseRepoUrl } from "../utils/common.js";
+
 export default async function handler(req, res) {
     try {
         const { repo } = req.query;
 
         if (!repo) {
-            throw new Error('Repository URL parameter is required.');
+            return res.status(400).json({ error: 'Repository URL parameter is required.' });
         }
 
-        const match = repo.match(/github\.com\/([^/]+)\/([^/]+)/);
-        if (!match) {
-            throw new Error('Invalid repository URL format.');
-        }
+        checkValidRepo(repo);
 
-        const repoOwner = match[1];
-        const repoSlug = match[2];
+        const repoOwner = parseRepoUrl(repo).owner;
+        const repoSlug = parseRepoUrl(repo).repo;
 
         const repoUrl = `https://api.github.com/repos/${repoOwner}/${repoSlug}/contents`;
         console.log(`Fetching repository contents from: ${repoUrl}`);
@@ -29,7 +28,7 @@ export default async function handler(req, res) {
         const response = await fetch(repoUrl, { headers });
 
         if (!response.ok) {
-            throw new Error(`Failed to fetch repository contents: ${response.statusText}`);
+            return res.status(response.status).json({ error: `Failed to fetch repository contents: ${response.statusText}` });
         }
 
         const files = await response.json();
